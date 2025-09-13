13738fb8764d5da2513844adc04006d8ac8aa99e
alimansourilearning <ali.mansourilearning@gmail.com>
2025-09-13 22:27:02 +0330

    type annotation & primitive & any & unknown

---
+++ app.ts
@@ -0,0 +1,41 @@
+let myName: string = "ali";
+// let myName = "ali";
+
+let firstname: string = "ali";
+
+let age: number = 25;
+let pi: number = 3.14;
+
+let isStudent: boolean = true;
+
+let names: string[] = ["ali", "mohammad", "hassan"];
+
+let scores: Array<number> = [100, 200, 300];
+
+let data: any = "hello";
+data = 10;
+data = true;
+
+let value: unknown = "thanks";
+value = 5;
+value = null;
+
+function f1(a: any) {
+  return a.b;
+}
+
+// function f2(a: unknown) {
+//   return a.b;
+// }
+
+function f2(a: unknown) {
+  if (typeof a === "object" && a !== null && "b" in a) {
+    return a.b;
+  }
+}
+
+let someUnknown: unknown = "hello";
+// let someString: string = someUnknown;
+
+let someAny: any = "thanks";
+let someNumber: number = someAny;
