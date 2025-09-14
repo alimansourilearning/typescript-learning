445e8d60933e5e1534f0041d5a0e2ddd16a553cf
alimansourilearning <ali.mansourilearning@gmail.com>
2025-09-14 16:14:08 +0330

    type assertion

---
+++ type-assertion.ts
@@ -0,0 +1,6 @@
+const myElement = document.getElementById("abc123") as HTMLButtonElement;
+myElement.disabled = true;
+
+// const x = "hello" as number
+const a = "dldklsf;kdfdlkfl;d"
+const b = a as any as number
