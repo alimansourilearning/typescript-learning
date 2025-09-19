2fea21b51ee2d4bc57cfa2ab6139211de9ac8756
alimansourilearning <ali.mansourilearning@gmail.com>
2025-09-19 21:47:38 +0330

    additional code for better explain

---
+++ additional.js
@@ -0,0 +1,11 @@
+function printMyName(name) {
+  console.log(name.toUpperCase());
+}
+
+printMyName({ name: "ali" });
+
+let myName = "ali";
+let age = 20;
+let isStudent = true;
+
+console.log(typeof myName, typeof age, typeof isStudent);
--- additional.ts
+++ additional.ts
@@ -1,1 +1,10 @@
+
+function printMyFirstname(name: string) {
+  console.log(name.toUpperCase());
+}
+
+// printMyFirstname({name: "ali"})
+let newValue = "hello";
+let myMessage: string = "Hello"
+
 let scores: Array<number> = [100, 200, 300];
